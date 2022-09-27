27bbc331f1e7f017940e5296736c0937a025c637
Jacob Do <jekabs.donis@gmail.com>
2022-09-27 14:02:50 +0200

    init commit

---
+++ fixtures/users.ts
@@ -0,0 +1,23 @@
+// users data
+export const USERS = [
+  {
+    id: 1,
+    name: "John Doe",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+  },
+];
+
+export const ALL_USERS = [
+  ...USERS,
+  {
+    id: 3,
+    name: "Tommy",
+  },
+  {
+    id: 4,
+    name: "Timmy",
+  },
+];
